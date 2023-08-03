2b522461c26b9cba65701253e96e191237facab8
Marcelino Garcia <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2023-08-03 01:38:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const port = 3333;
 app.use(express.json());
 app.listen(3333, () => { console.log(`Run server...${port}`) });
 
-app.get('/', (req,res) => res.send("API-USER"));
-
 app.post('/user', userController.createUser);
 app.get('/user', userController.findUsers);
 
