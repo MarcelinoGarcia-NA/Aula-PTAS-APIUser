6d90f4f0742ca00def64b9bcc1fdeede40e67862
Marcelino Garcia <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2023-08-03 01:55:19 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const connectionDataBase = require('./config/connection');
 const userController = require('./controller/userController');
 const express = require('express');
 const app = express();
-const port = 3333;
+const port = 3003;
 app.use(express.json());
-app.listen(3333, () => { console.log(`Run server...${port}`) });
+app.listen(port, () => { console.log(`Run server...${port}`) });
+
+app.get('/', (req,res) => res.send("API-USER"));
 
 app.post('/user', userController.createUser);
 app.get('/user', userController.findUsers);
