315d6203284940cc588a2a4b8272aa0f758e9e6a
Marcelino Garcia <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2023-08-03 01:07:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const connectionDataBase = require('./config/connection');
 const userController = require('./controller/userController');
 const express = require('express');
 const app = express();
-const port = 3003;
+const port = 3000;
 app.use(express.json());
 app.listen(port, () => { console.log(`Run server...${port}`) });
 
